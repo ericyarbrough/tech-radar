363ce5ddc82b9d3172daa997f954a40449176d74
Yarbrough, Eric <enyarbrough@gmail.com>
2021-11-16 01:58:34 -0500

    rebuild and autofix issues

--- src/data.js
+++ src/data.js
@@ -1,90 +1,115 @@
+const quadrants = {
+  LANGUAGES: 0,
+  INFRASTRUCTURE: 1,
+  TOOLS: 2,
+  PLATFORM: 3,
+};
+
 const initialRadarData = {
-  svg_id: "radar",
+  svg_id: 'radar',
   width: 1450,
   height: 1000,
   colors: {
-    background: "#fff",
-    grid: "#bbb",
-    inactive: "#ddd"
+    background: '#eee',
+    grid: '#bbb',
+    inactive: '#ddd',
   },
-  title: "Tech Radar",
+  title: 'Tech Radar',
   quadrants: [
-    { name: "Languages" },
-    { name: "Infrastructure" },
-    { name: "Tools" },
-    { name: "Platforms" },
+    { name: 'Languages' },
+    { name: 'Infrastructure' },
+    { name: 'Tools' },
+    { name: 'Platforms' },
   ],
   rings: [
-    { name: "ADOPT", color: "#93c47d" },
-    { name: "TRIAL", color: "#93d2c2" },
-    { name: "ASSESS", color: "#fbdb84" },
-    { name: "HOLD", color: "#efafa9" }
+    { name: 'ADOPT', color: '#93c47d' },
+    { name: 'TRIAL', color: '#93d2c2' },
+    { name: 'ASSESS', color: '#fbdb84' },
+    { name: 'HOLD', color: '#efafa9' },
   ],
   print_layout: true,
   // zoomed_quadrant: 0,
   entries: [
-      {
-        "quadrant": 1,
-        "ring": 2,
-        "label": "OpenAPM.io",
-        "moved": 0
-      },
-      {
-        "quadrant": 1,
-        "ring": 0,
-        "label": "Swagger",
-        "moved": 0
-      },
-      {
-        "quadrant": 3,
-        "ring": 1,
-        "label": "Next.js",
-        "moved": 0
-      },
-      {
-        "quadrant": 2,
-        "ring": 0,
-        "label": "Jest",
-        "moved": 0
-      },
-      {
-        "quadrant": 2,
-        "ring": 0,
-        "label": "MariaDB",
-        "moved": 0
-      },
-      {
-        "quadrant": 2,
-        "ring": 0,
-        "label": "Redis",
-        "moved": 0
-      },
-      {
-        "quadrant": 2,
-        "ring": 1,
-        "label": "MongoDB",
-        "moved": 0
-      },
-      {
-        "quadrant": 2,
-        "ring": 1,
-        "label": "Postgres",
-        "moved": 0
-      },
-      {
-        "quadrant": 2,
-        "ring": 2,
-        "label": "Cassandra",
-        "moved": 0
-      },
-      {
-        "quadrant": 2,
-        "ring": 2,
-        "label": "Scylladb",
-        "moved": 0
-      },
-  ]
-  //ENTRIES
-}
+    {
+      quadrant: quadrants.INFRASTRUCTURE,
+      ring: 1,
+      label: 'OpenAPM.io',
+      moved: 0,
+    },
+    {
+      quadrant: quadrants.INFRASTRUCTURE,
+      ring: 0,
+      label: 'Swagger',
+      moved: 0,
+    },
+    {
+      quadrant: quadrants.INFRASTRUCTURE,
+      ring: 0,
+      label: 'Terraform',
+      moved: 0,
+    },
+    {
+      quadrant: quadrants.INFRASTRUCTURE,
+      ring: 0,
+      label: 'Kubernetes',
+      moved: 0,
+    },
+    {
+      quadrant: quadrants.INFRASTRUCTURE,
+      ring: 1,
+      label: 'Nomad',
+      moved: 0,
+    },
+    {
+      quadrant: 3,
+      ring: 1,
+      label: 'Next.js',
+      moved: 0,
+    },
+    {
+      quadrant: 2,
+      ring: 0,
+      label: 'Jest',
+      moved: 0,
+    },
+    {
+      quadrant: 2,
+      ring: 0,
+      label: 'MariaDB',
+      moved: 0,
+    },
+    {
+      quadrant: 2,
+      ring: 0,
+      label: 'Redis',
+      moved: 0,
+    },
+    {
+      quadrant: 2,
+      ring: 1,
+      label: 'MongoDB',
+      moved: 0,
+    },
+    {
+      quadrant: 2,
+      ring: 1,
+      label: 'Postgres',
+      moved: 0,
+    },
+    {
+      quadrant: 2,
+      ring: 2,
+      label: 'Cassandra',
+      moved: 0,
+    },
+    {
+      quadrant: 2,
+      ring: 2,
+      label: 'Scylladb',
+      moved: 0,
+    },
+  ],
+  // ENTRIES
+};
 
 export default initialRadarData;
